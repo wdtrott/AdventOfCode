5e8355214371f20a69a0504146ea13a324a1067e
wdtrott <102983715+wdtrott@users.noreply.github.com>
2022-12-01 12:33:24 -0600

    Python answer for day 1 of the Advent of Code

---
+++ 1201.py
@@ -0,0 +1,16 @@
+with open('/Users/wtrott/Downloads/1201input.txt') as file:
+    data = [line.strip() for line in file]
+    
+iter = 1
+elves = {1: []}
+for line in data:
+    if line:
+        elves[iter].append(int(line))
+    else:
+        iter += 1
+        elves[iter] = []
+
+sort_cals = dict(sorted({elf: sum(calories) for elf, calories in elves.items()}.items(), key=lambda item: item[1]))
+
+print(f"Max calories: {total_cals[list(sort_cals.keys())[-1]]}")
+print(f"Total for top 3 calories: {sum([total_cals[key] for key in list(sort_cals.keys())[-3:]])}")
